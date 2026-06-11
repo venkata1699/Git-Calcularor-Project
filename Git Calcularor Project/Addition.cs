7d2997aab6a5ec41501b2d34d0a494e6e44bf22a
venkata1699 <ramana.r1699@gmail.com>
2026-06-11 11:07:35 +0530

    Add project files.

---
+++ Git Calcularor Project/Addition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Calcularor_Project
+{
+    internal class Addition
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Git Calcularor Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Calcularor_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Addition addition = new Addition();  
+           double A =  addition .Add(5, 10);
+            Console.WriteLine("The result of addition is: " + A);
+
+            Substraction substraction = new Substraction(); 
+             double B =   substraction .Subtract(10, 5);
+            Console.WriteLine("The result of substraction is: " + B);
+        }
+    } 
+}
