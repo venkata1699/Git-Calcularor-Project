25f56a26c67536c6335cc5be142fd9a04db01b1f
venkata1699 <ramana.r1699@gmail.com>
2026-06-11 11:30:50 +0530

    division is added

---
+++ Git Calcularor Project/division.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Calcularor_Project
+{
+    internal class division
+    {
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+                return double.NaN; // Return Not-a-Number to indicate an error
+            }
+            return num1 / num2;
+        }
+    }
+}
