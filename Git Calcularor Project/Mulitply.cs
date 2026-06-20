fcb1d94d87cc7e1e07881b0a97ca3b9bc23bd579
venkata1699 <ramana.r1699@gmail.com>
2026-06-20 11:58:30 +0530

    This is i am Cahnging

--- Git Calcularor Project/Mulitply.cs
+++ Git Calcularor Project/Mulitply.cs
@@ -8,9 +8,9 @@ namespace Git_Calcularor_Project
 {
     internal class Mulitply
     {
-        //public double Add(double num1, double num2)
-        //{
-        //    return num1 + num2;
-        //}
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
     }
 }
